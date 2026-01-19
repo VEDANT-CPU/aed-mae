b823078630eab22479ff94637fd7fa06590283c0
VEDANT-CPU <vedantpawar896@gmail.com>
2026-01-19 18:12:17 +0530

    modifying epochs

--- configs/configs.py
+++ configs/configs.py
@@ -4,7 +4,7 @@ import ml_collections
 def get_configs_avenue():
     config = ml_collections.ConfigDict()
     config.batch_size = 100
-    config.epochs = 200
+    config.epochs = 50
     config.mask_ratio = 0.5
     config.start_TS_epoch = 100
     config.masking_method = "random_masking"
@@ -25,43 +25,6 @@ def get_configs_avenue():
     config.dataset = "avenue"
     config.avenue_path = "/content/drive/MyDrive/Colab Notebooks/aed-mae/Datasets/frames_data"
     config.avenue_gt_path = "/content/drive/MyDrive/Colab Notebooks/aed-mae/Datasets/fd_gt"
-    config.percent_abnormal = 0.25
-    config.input_3d = True
-    config.device = "cuda"
-
-    config.start_epoch = 0
-    config.print_freq = 10
-    config.num_workers = 10
-    config.pin_mem = False
-
-    return config
-
-
-#def get_configs_shanghai():
-#    config = ml_collections.ConfigDict()
-#    config.batch_size = 100
-#    config.epochs = 200
-#    config.mask_ratio = 0.5
-#    config.start_TS_epoch = 100
-#    config.masking_method = "random_masking"
-#    config.output_dir = "experiments/shanghai" # the checkpoints will be loaded from here
-#    config.abnormal_score_func = 'L1'
-#    config.grad_weighted_rec_loss = True
-#    config.model = "mae_cvt"
-#    config.input_size = (160, 320)
-    config.norm_pix_loss = False
-    config.use_only_masked_tokens_ab = False
-    config.run_type = "train"
-    config.resume=False
-
-    # Optimizer parameters
-    config.weight_decay = 0.05
-    config.lr = 1e-4
-
-    # Dataset parameters
-    config.dataset = "shanghai"
-    config.shanghai_path = "/media/alin/hdd/SanhaiTech"
-    config.shanghai_gt_path = "/media/alin/hdd/Transformer_Labels/Shanghai_gt"
     config.percent_abnormal = 0.25
     config.input_3d = True
     config.device = "cuda"
